489f83184d2c7ed29e3e48f8d3e719879b539500
André Antunes <andrepalhares1989@gmail.com>
2018-11-24 22:57:43 -0200

    Finished section 9(JS) started Responsive design Section 10

--- shared.js
+++ shared.js
@@ -1,24 +1,50 @@
 let backdrop = document.querySelector('.backdrop');
 let button = document.querySelectorAll('.plan button');
+// Modal Selectors//
 let modal = document.querySelector('.modal');
+let modalNoBtn = document.querySelector('.modal__action--negative');
 
-/*
-for(var i = 0; i <= button.length; i++) {
-    button[i].addEventListener('click', function() {
+// Toggle Button Selectors//
+let mobileNav = document.querySelector('.mobile-nav');
+let toggleBtn = document.querySelector('.toggle-button');
 
-    });
+
+
+
+function displayModal() {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open' this will overwrite the complete element
+    modal.classList.add('open');
+    backdrop.classList.add('open');
 }
-*/
+
+function closeModal() {
+    // modal.style.display = "none";
+    // backdrop.style.display = "none";
+    if(modal){
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
+}
 
 button.forEach(function(i) {
-    i.addEventListener('click', () => {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
-    })
-})
+    i.addEventListener("click", displayModal)
+});
 
-let modalBtnNegative = document.querySelector('.modal__action--negative');
-modalBtnNegative.addEventListener('click', () => {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+if(modalNoBtn) {
+    modalNoBtn.addEventListener("click", closeModal);
+}
+
+backdrop.addEventListener("click", () => {
+    // mobileNav.style.display = "none";
+    closeModal();
+    mobileNav.classList.remove('open');
+});
+
+toggleBtn.addEventListener("click", () => {
+    // backdrop.style.display = "block";
+    // mobileNav.style.display = "block";
+    backdrop.classList.add('open');
+    mobileNav.classList.add('open');
 });
